d92a047864dfcea8b805570c61b3f40412bc8af7
Anastassia2312 <23122000na@gmail.com>
2024-02-25 09:58:44 +0200

    S

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,9 @@
 import { FaUser } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
+import css from "./Contact.module.css";
 export default function Contact({ info, onDelete }) {
   return (
-    <div>
+    <div className={css.wrapper}>
       <p>
         <FaUser />
         {info.name}
