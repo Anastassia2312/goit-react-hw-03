39f2ce5bdbe8ab8678182199c382b74397c1612c
Anastassia2312 <23122000na@gmail.com>
2024-02-25 17:07:05 +0200

    summary

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,14 +4,16 @@ import css from "./Contact.module.css";
 export default function Contact({ info, onDelete }) {
   return (
     <div className={css.box}>
-      <p className={css.string}>
-        <FaUser />
-        {info.name}
-      </p>
-      <p className={css.string}>
-        <FaPhoneAlt />
-        {info.number}
-      </p>
+      <div>
+        <p className={css.string}>
+          <FaUser />
+          {info.name}
+        </p>
+        <p className={css.string}>
+          <FaPhoneAlt />
+          {info.number}
+        </p>
+      </div>
       <button onClick={() => onDelete(info.id)}>Delete</button>
     </div>
   );
